ba2c6961c347984cef58690f297bd73f50ed9f7c
Oscarkindvall <62562054+Oscarkindvall@users.noreply.github.com>
2021-05-18 17:10:05 +0200

    Initial API fetch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import { Dimmer, Loader } from 'semantic-ui-react';
 
 function App() {
+
+const [load, setLoad] = useState(true);
+const [price, setPrice] = useState(null);
+const [currency, setCurrency] = useState("USD");
+
+
+useEffect(() => {
+  async function fetchData() {
+    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    const data = await res.json();
+    console.log(res)
+    console.log(data)
+    console.log(data.bpi)
+    setPrice(data.bpi);
+    setCurrency("EUR");
+    setLoad(false);
+  }
+  fetchData();
+}, []);
+
+  // const handleSelect = (e, data) => {
+  //   setCurrency(data.value);
+  // }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bitcoin Diary</h1>
+        {
+          load ? (
+            <div>
+            <Dimmer active inverted>
+              <Loader>
+                Loading
+              </Loader>
+            </Dimmer>
+            </div>
+          ) : (
+            <div className="price-container">
+              <h1>{currency}</h1>
+              <h2>{price[currency].rate}</h2>
+            </div>
+          )
+        }
     </div>
   );
 }
