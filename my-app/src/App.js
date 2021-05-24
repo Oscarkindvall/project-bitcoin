463ecce5135629eb47e3a83f21cb48ea78429e97
Filip-M <filip.moltzer@gmail.com>
2021-05-24 21:49:59 +0200

    Chart UI created with apexcharts
    apexcharts needs to be npm installed locally before running.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
 import React, {useState, useEffect} from "react";
-import { Dimmer, Loader } from 'semantic-ui-react';
+import { Dimmer, Loader, Select, Card} from 'semantic-ui-react';
+import Chart from "react-apexcharts";
+
+const options = [
+{value: "USD", text: "USD"},
+{value: "EUR", text: "EUR"},
+{value: "GBP", text: "GBP"}
+]
 
 function App() {
 
 const [load, setLoad] = useState(true);
 const [price, setPrice] = useState(null);
 const [currency, setCurrency] = useState("USD");
+const [chartData, setChartData] = useState(null);
+const [series, setSeries] = useState(null);
 
 
 useEffect(() => {
@@ -17,15 +26,38 @@ useEffect(() => {
     console.log(data.bpi)
     setPrice(data.bpi);
     setCurrency("EUR");
-    setLoad(false);
+    getChartData();
   }
   fetchData();
 }, []);
 
-  // const handleSelect = (e, data) => {
-  //   setCurrency(data.value);
-  // }
+  const handleSelect = (e, data) => {
+    setCurrency(data.value);
+  }
 
+  const getChartData = async () => {
+    const res = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
+    const data = await res.json();
+    const categories = Object.keys(data.bpi); //Turn it into an array of Keys.
+    const series = Object.values(data.bpi);
+    setChartData({
+      xaxis: {
+        categories:categories
+      }
+    })
+
+    setSeries([
+      {
+      name: "Bitcoin Price",
+      data: series
+      }
+    ])
+
+    setLoad(false);
+
+  }
+
+   
   return (
 
     <div className="App">
@@ -41,8 +73,25 @@ useEffect(() => {
             </div>
           ) : (
             <div className="price-container">
-              <h1>{currency}</h1>
-              <h2>{price[currency].rate}</h2>
+              <Select placeholder="Select currency" 
+              onChange={handleSelect} 
+              options={options}
+              ></Select>
+
+              <Card>
+                <Card.Content>
+                  <Card.Header>{currency}</Card.Header>
+                  <Card.Description>{price[currency].rate}</Card.Description>
+                </Card.Content>
+
+              </Card>
+
+              <Chart options={chartData}
+              series={series}
+              type="line"
+              width="1200"
+              height="300"/>
+
             </div>
           )
         }
