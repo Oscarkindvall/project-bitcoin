7fcb1a18f5942b3b0e216233440f94a1927a6ab8
Oscarkindvall <62562054+Oscarkindvall@users.noreply.github.com>
2021-06-03 10:52:33 +0200

    erased outcomment code and unused imports etc

--- my-app/src/article.js
+++ my-app/src/article.js
@@ -1,115 +1,18 @@
-import React, {useState} from 'react';
-import styles from './style.css';
 
-
-let key = "EpxB0D2US4cMY5RWaAlssDVQ1SQ6nefh";
-// pub_date	Timestamp (YYYY-MM-DD)
-// let date = "2021-05-20"
-// pub_date=2021-05-06
-
-
-// const getArticle = async () => {
-//     const res = await fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=bitcoin&sort=newest&api-key='+key);
-//     const data = await res.json();
-//     console.log(data);
-//     const headline = data.response.docs[0].headline.main;
-//     const pubDate = data.response.docs[0].pub_date;
-//     const webUrl = data.response.docs[0].web_url;
-//     const abstract = data.response.docs[0].abstract;
-//     const articleImg = data.response.docs[0].multimedia[0].url;
-
-//     return {headline,pubDate,webUrl,abstract,articleImg}
-//   }
-
-
-  
 export default function Article(props) {
-  console.log(props);
   const url = "http://www.nytimes.com/";
-
-  // let article = [];
-
-  // const [article, setArticle] = useState(
-  //   {
-  //     headline: "", 
-  //     pubDate:  "",
-  //     webUrl:  "", 
-  //     abstract:  "",
-  //     articleImg: ""
-  //   }
-  // );
   
-  // function createArticle(result)  {
-  //   return {
-  //   headline: result.headline,
-  //   pubDate: result.pubDate,
-  //   webUrl: result.webUrl,
-  //   abstract: result.abstract,
-  //   articleImg: result.articleImg
-  //   }
-  // }
-  
-
-  // const fetchArticle = getArticle();
-  // fetchArticle.then(function(result) {
-    
-
-  //   console.log(result);
-  //   console.log(result.headline);
-  //   console.log(result.articleImg);
-  //   console.log(result.pubDate);
-  //   console.log(result.webUrl);
-  //   console.log(result.abstract);
-  //   article.push(result);
-
-  //   // for(let i=0;i<article.length;i++)
-  //   // {
-  //   // let articles = article[i];
-  //   // return articles;
-  //   // }
-
-  //   // {}?
-
-  //   // setArticle({
-  //   //   headline: result.headline,
-  //   //   pubDate: result.pubDate,
-  //   //   webUrl: result.webUrl,
-  //   //   abstract: result.abstract,
-  //   //   articleImg: result.articleImg
-  //   //   )}
-  //   // let ar = result
-  //   // return ar
-  //   })
-
- 
-    
-    // console.log(articles.headline);
-    
-    // console.log(article);
-    // console.log(article.map(a => a.headline))
-    // // console.log(ar);
-    // console.log(article.headline);
-    // // console.log(article.0);
-
-   
-
     return ( 
         <>
         <div className="card">
-          
-          <h2>Most recent article on NY Times mentioning <img id="bitcoinImg" src="./images/bitcoin.png"></img> </h2>
+          <h2>Most recent article on NY Times mentioning <img id="bitcoinImg" src="./images/bitcoin.png" alt="bitcoin logo"></img> </h2>
           <article className="inner-card"> 
-            {console.log(props)}
-            <div>{console.log(props.article)}</div>
-            <img id="NYT" src="./images/The_New_York_Times_logo.png"></img>
+            <img id="NYT" src="./images/The_New_York_Times_logo.png" alt="NY-Times logo"></img>
             <div><h3>{props.article.headline}</h3></div>
-            
-            
             <div><p>{props.article.abstract}</p></div>
-            <div><img src={url + props.article.articleImg}></img></div>
+            <div><img src={url + props.article.articleImg} alt={props.article.headline}></img></div>
             <div><p><strong>Publication date:</strong> {props.article.pubDate.slice(0,10)} </p></div>
-            <div><a href={props.article.webUrl}>Read more... </a></div>
-            {/* <div><img src={url + props.article.articleImg}</img></div> */}
+            <div><a href={props.article.webUrl} target="_blank" rel="noreferrer noopener">Read more... </a></div>
           </article>
         </div>
           </>
--- my-app/src/saveData.js
+++ my-app/src/saveData.js
@@ -1,161 +1,123 @@
 import React, { useRef, useState} from 'react';
-import { Statistic } from 'semantic-ui-react'
 import Portfolio from './portfolio';
 import DatePicker from "react-datepicker";
 import moment from 'moment';
 import "react-datepicker/dist/react-datepicker.css";
 
 
-
-
-
 const getPriceOnDate = async (date, amount) => {
-    const res = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json?start='+date+'&end='+date);
-    const data = await res.json();
- 
-    let buyDate = Object.keys(data.bpi);
-    let bitcoinValue = Object.values(data.bpi);
-    let buySize = amount;
-    let buyAmount = parseInt(bitcoinValue) * amount;
-    console.log(buyDate);
-    console.log(buyAmount);
-    
-    return {buyDate, buyAmount, bitcoinValue, buySize}
-  }
+  const res = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json?start='+date+'&end='+date);
+  const data = await res.json();
+
+  let buyDate = Object.keys(data.bpi);
+  let bitcoinValue = Object.values(data.bpi);
+  let buySize = amount;
+  let buyAmount = parseInt(bitcoinValue) * amount;
+  
+  return {buyDate, buyAmount, bitcoinValue, buySize}
+}
   
 
 export function LoadData() {
-    let localPortfolio= localStorage.getItem("portfolio");
-    if(localPortfolio == null){
-    //Exist data in local
-        localStorage.setItem("portfolio", JSON.stringify([]));
-        // Returnerar en tom lista (= inga todos)
-        return [];
-        
-    }else{
-    //Non Exist data block
-        return JSON.parse(localPortfolio);
-    }
+  let localPortfolio= localStorage.getItem("portfolio");
+  if(localPortfolio == null){
+      localStorage.setItem("portfolio", JSON.stringify([]));
+      return []; 
+  }else{
+      return JSON.parse(localPortfolio);
+  }
 }
 
 export default function SaveData(props) {
-    let localPortfolio = []
-    
-    LoadData().forEach(portObject => {
-      // Loops through each object from storage
-      localPortfolio.push(portObject)
-    });
-    
-    const [portfolio, setPortfolio] = useState(
-      // Declares localStorage object values as default for portfolio
-      localPortfolio
-    );
-   
-    // const dateRef = useRef();
+  let localPortfolio = []
+  
+  LoadData().forEach(portObject => {
+    // Loops through each object from storage
+    localPortfolio.push(portObject)
+  });
+  
+  const [portfolio, setPortfolio] = useState(
+    // Declares localStorage object values as default for portfolio
+    localPortfolio
+  );
+
     const amountRef = useRef();
-    // const [startDate, setStartDate] = useState(new Date());
+    const [startDate, setCurrentDate] = useState(new Date());
     
     
-    // const [startDate, setStartDate] = useState(new Date());
-    // console.log(startDate);
-
-    
-      const [startDate, setCurrentDate] = useState(new Date());
-      console.log(startDate);
-
-      // const today = moment();
-      // const disableFutureDate = current => {
-      // return current.isBefore(today)
-      // }
-      
-      function formatDate(date) {
-        JSON.stringify(date);
-        let str = JSON.stringify(date);
-        let dateArray = [];
-        dateArray = str.split(' ').map(function (word) {
-          return word
-        })
-        const newDate = dateArray[0].slice(1,11)
-        console.log(newDate)
-        // if(newDate > new Date) {
-        //   alert("nej");
-        // } else {
-        setCurrentDate(newDate)
-        // }
-        
-      
-      //   // date.slice(4, 15);
-      //   console.log(date)
-      //   setCurrentDate(date)
-      //   // console.log(format(new Date(), 'yyyy-LL-dd')); // 2019-08-23
-      }
-
-
-    function addCoin(e) {
-      
-      if (isNaN(parseInt(amountRef.current.value)) != true) {
-        if (parseInt(amountRef.current.value) != 0) {
-            if (startDate != formatDate(new Date())) {
-            function saveToLocal(result) {
-              let Local = LoadData();
-              Local.push({date: result.buyDate[0], amount: result.buyAmount, buySize: result.buySize, bitcoinValue: result.bitcoinValue[0]});
-              localStorage.setItem("portfolio", JSON.stringify(Local));
-            }
-
-            console.log(startDate);
-            let priceResult = getPriceOnDate(startDate, amountRef.current.value);
-            priceResult.then(function(result) {
-              saveToLocal(result)
-              setPortfolio([...portfolio, {
-                date: result.buyDate[0], 
-                amount: result.buyAmount,
-                buySize: result.buySize, 
-                bitcoinValue: result.bitcoinValue[0]
-              }]);
-            })
-          amountRef.current.value = "";
-            }
-            else {alert("Todays history price is'nt available yet... Please choose an earlier date than today.")}
-          }
-        else {alert("Amount of Bitcoins has to be above 0")}
-      }
-      else {alert("Amount of Bitcoins has to be inserted as a number")}
+    function formatDate(date) {
+      JSON.stringify(date);
+      let str = JSON.stringify(date);
+      let dateArray = [];
+      dateArray = str.split(' ').map(function (word) {
+        return word
+      })
+      const newDate = dateArray[0].slice(1,11)
+      setCurrentDate(newDate)
+      return newDate
     }
+
+  function addCoin(e) {
+    
+    if (isNaN(parseInt(amountRef.current.value)) !== true) {
+      if (parseInt(amountRef.current.value) !== 0) {
+        if (startDate !== formatDate(new Date())) {
+          function saveToLocal(result) {
+          let Local = LoadData();
+          Local.push({date: result.buyDate[0], amount: result.buyAmount, buySize: result.buySize, bitcoinValue: result.bitcoinValue[0]});
+          localStorage.setItem("portfolio", JSON.stringify(Local));
+          }
+
+          let priceResult = getPriceOnDate(startDate, amountRef.current.value);
+          priceResult.then(function(result) {
+            saveToLocal(result)
+            setPortfolio([...portfolio, {
+              date: result.buyDate[0], 
+              amount: result.buyAmount,
+              buySize: result.buySize, 
+              bitcoinValue: result.bitcoinValue[0]
+            }]);
+          })
+            amountRef.current.value = "";
+          }
+          else {alert("Todays history price is'nt available yet... Please choose an earlier date than today.")}
+        }
+      else {alert("Amount of Bitcoins has to be above 0")}
+    }
+    else {alert("Amount of Bitcoins has to be inserted as a number")}
+  }
           
 
-        return (
-          <>
-            <div>
-            <h2>Bitcoin Portfolio</h2>
-            <form id="log-buy" >
-                {/* Add className="ui input error" when wrong input */}
-                <div class="ui right labeled input form-inputs">
-                  <input type="text" id="amount" className="form-control" placeholder="Amount..." ref={amountRef}/>
-                  <div class="ui basic label">
-                      Bitcoin
-                  </div>
-                </div>
-
-                <div class="ui right labeled input">
-                  <div class="ui basic label">
-                      Date
-                  </div>
-                  <DatePicker maxDate={moment().toDate()} selected={new Date()} onChange={date => formatDate(date)} />
-                </div>
-
-            </form>
-            <input onClick={addCoin} type="submit" className="btn btn-success mt-3 ui inverted green button form-submit" value="Registrera köp" />
+  return (
+    <>
+      <div>
+      <h2>Bitcoin Portfolio</h2>
+      <form id="log-buy" >
+          <div class="ui right labeled input form-inputs">
+            <input type="text" id="amount" className="form-control" placeholder="Amount..." ref={amountRef}/>
+            <div class="ui basic label">
+                Bitcoin
             </div>
+          </div>
 
-            
-            <div> 
-              < div className="portContanier">
-                {<Portfolio portfolio={portfolio} price={props.price}/> }
-              </div>
+          <div class="ui right labeled input">
+            <div class="ui basic label">
+                Date
             </div>
-            </>
-            )            
-    }
+            <DatePicker maxDate={moment().toDate()} selected={new Date()} onChange={date => formatDate(date)} />
+          </div>
 
-    
-   
+      </form>
+      <input onClick={addCoin} type="submit" className="btn btn-success mt-3 ui inverted green button form-submit" value="Registrera köp" />
+      </div>
+
+      <div> 
+        < div className="portContanier">
+          {<Portfolio portfolio={portfolio} price={props.price}/> }
+        </div>
+      </div>
+      </>
+      )            
+}
+
+
